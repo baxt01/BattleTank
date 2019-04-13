df4b2dc4d4c78cb561b83a865db5fff7b43f0190
baxt01 <baxt01@hotmail.com>
2019-04-13 07:00:20 +0100

    added AI Tank finding the player Tank
    I have added the ability for the AI Tanks to be able to locate the Player Tank within the world in C++

---
+++ Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+#include "BattleTank.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI controller cant find player tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI controller found: %s"), *(PlayerTank->GetName()));
+	}
+}
+
+ATank* ATankAIController::GetControledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) { return nullptr; }
+	return Cast<ATank>(PlayerPawn);
+}
